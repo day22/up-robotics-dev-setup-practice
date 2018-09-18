59dcec78cc4f510d8a13fe9962d3d60fa985b44d
Aidan Day <day22@up.edu>
2018-09-17 19:25:04 -0700

    this better work

--- src/main.py
+++ src/main.py
@@ -15,7 +15,7 @@
 # a very basic main method. TODO: new people add new things please
 def main():
     print ( "I did a thing")
-
+    print ( "finally" ) 
 
 # Python magic to make the main method run 
 if __name__ == "__main__":
